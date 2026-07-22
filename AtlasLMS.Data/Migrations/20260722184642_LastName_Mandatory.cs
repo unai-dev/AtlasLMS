bee0d16dab666f2d65836f29cb49ae644e49a4da
Unai Villar <unaidevcoding@gmail.com>
2026-07-22 21:30:08 +0200

    Fix: se quita la anchura fija en formulario

---
+++ AtlasLMS.Data/Migrations/20260722184642_LastName_Mandatory.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AtlasLMS.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class LastName_Mandatory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Authors",
+                type: "nvarchar(55)",
+                maxLength: 55,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(55)",
+                oldMaxLength: 55,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Authors",
+                type: "nvarchar(55)",
+                maxLength: 55,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(55)",
+                oldMaxLength: 55);
+        }
+    }
+}
