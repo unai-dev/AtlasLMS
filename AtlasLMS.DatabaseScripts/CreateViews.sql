88af4ad0d8bd0a91fe5220babc647a2c494bad07
Unai Villar <unaidevcoding@gmail.com>
2026-07-26 18:27:58 +0200

    Improvement: se agregan nuevas vistas para tablas usuarios y prestamos

--- AtlasLMS.DatabaseScripts/CreateViews.sql
+++ AtlasLMS.DatabaseScripts/CreateViews.sql
@@ -36,3 +36,32 @@ CREATE OR ALTER VIEW [dbo].[vw_Categories] AS
 	FROM Categories AS C
 	INNER JOIN Books AS B ON C.ID = B.CategoryID
 GO
+
+CREATE OR ALTER VIEW [dbo].[vw_Loans] AS
+	SELECT L.*
+
+	,U.UserName AS [UserName]
+	,U.Email AS [UserEmail]
+	,U.CIF AS [UserCIF]
+
+	,B.ISBN AS [BookISBN]
+	,B.Title AS [BookTitle]
+
+	FROM Loans AS L
+	INNER JOIN AspNetUsers AS U ON L.UserID = U.Id
+	INNER JOIN Books AS B ON L.BookID = B.ID
+GO
+	
+CREATE OR ALTER VIEW [dbo].[vwg_Users] AS
+	SELECT U.*
+
+	,L.ID AS [LoanID]
+	
+	,B.ID AS [BookingID]
+
+
+	FROM  AspNetUsers AS U
+	LEFT JOIN Loans AS L ON U.Id = L.UserID
+	LEFT JOIN Bookings AS B ON U.Id = B.UserID
+GO
+	
