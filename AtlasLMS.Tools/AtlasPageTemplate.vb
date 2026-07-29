5446d9dcc77251b6c234d3d64c60296ba01bd6e2
Unai Villar <unaidevcoding@gmail.com>
2026-07-29 12:48:24 +0200

    Improvement: se agrega template para paginas

---
+++ AtlasLMS.Tools/AtlasPageTemplate.vb
@@ -0,0 +1,22 @@
+﻿Public Class AtlasPageTemplate
+    Public sPageTemplate As String =
+"@code {
+    //PROPS
+
+    #region OnInitialized----------------------------------------------------------------
+    protected override async Task OnInitializedAsync()
+    {
+        //CODE
+    }
+    #endregion
+
+    #region ButtonActions----------------------------------------------------------------
+        //CODE
+
+    #endregion
+
+    #region Methods----------------------------------------------------------------------
+        //CODE
+    #endregion
+}"
+End Class
