512c4cf25905e8eb588a4adee48585bf48cc07e1
Unai Villar <unaidevcoding@gmail.com>
2026-07-29 13:24:44 +0200

    Development: se desarrolla pagina de ubicaciónes

---
+++ AtlasLMS.Blazor/Features/Locations/Contracts/ILocationService.cs
@@ -0,0 +1,10 @@
+﻿using AtlasLMS.Shared.DTOs.Read;
+
+namespace AtlasLMS.Blazor.Features.Locations.Contracts
+{
+    public interface ILocationService
+    {
+        Task<IEnumerable<LocationReadDto>> GetLocationsAsync();
+        Task DeleteLocationAsync(int ID);
+    }
+}
---
+++ AtlasLMS.Blazor/Features/Locations/Services/LocationService.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Http.Json;
+
+using AtlasLMS.Blazor.Features.Locations.Contracts;
+using AtlasLMS.Shared.DTOs.Read;
+
+namespace AtlasLMS.Blazor.Features.Locations.Services;
+
+public class LocationService : ILocationService
+{
+    private readonly HttpClient _http;
+
+    public LocationService(HttpClient http)
+    {
+        _http = http;
+    }
+
+    public async Task<IEnumerable<LocationReadDto>> GetLocationsAsync() =>
+        await _http.GetFromJsonAsync<IEnumerable<LocationReadDto>>("locations") ?? [];
+    public async Task DeleteLocationAsync(int ID) =>
+        await _http.DeleteAsync($"locations/{ID}");
+}
