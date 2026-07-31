a658aa074eb9bdc2ceda1e105d8e8968fc2c6e89
Unai Villar <unaidevcoding@gmail.com>
2026-07-31 13:30:59 +0200

    Development: se desarrolla formulario generico

---
+++ AtlasLMS.Blazor/Shared/Components/AtlasForm.razor.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace AtlasLMS.Blazor.Shared.Components;
+
+public partial class AtlasForm<TModel>
+{
+    #region Parameters---------------------------------------------------------
+    [Parameter, EditorRequired] public TModel Model { get; set; }
+    [Parameter, EditorRequired] public EventCallback<TModel> OnSave { get; set; }
+    [Parameter, EditorRequired] public EventCallback OnCancel { get; set; }
+    [Parameter, EditorRequired] public RenderFragment ChildContent { get; set; }
+    [Parameter, EditorRequired] public string CaptionMaintenance { get; set; }
+    #endregion
+
+    private EditContext? editContext;
+
+    #region OnInitialized-------------------------------------------------
+    protected override void OnInitialized()
+    {
+        editContext = new EditContext(Model);
+    }
+    #endregion
+
+    #region ButtonActions----------------------------------------------------------
+    private async Task HandleValidSubmit()
+    {
+        if (editContext is not null && editContext.Validate())
+            await OnSave.InvokeAsync(Model);
+    }
+    #endregion
+}
