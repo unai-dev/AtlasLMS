0e24f974fe0fdcda1f100835b78a773d57774234
Unai Villar López <unaidevcoding@gmail.com>
2026-08-02 18:40:34 +0200

    Improvement: migracion completada

---
+++ AtlasLMS.Data/Migrations/20260802163903_RemoveNullableStock.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AtlasLMS.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveNullableStock : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Stock",
+                table: "Books",
+                type: "int",
+                nullable: false,
+                defaultValue: 1,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true,
+                oldDefaultValue: 1);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Stock",
+                table: "Books",
+                type: "int",
+                nullable: true,
+                defaultValue: 1,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldDefaultValue: 1);
+        }
+    }
+}
