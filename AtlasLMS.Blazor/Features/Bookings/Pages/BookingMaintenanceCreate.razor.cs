5287493bd4171dd5dafa26ace4021d431b202b3f
Unai Villar López <unaidevcoding@gmail.com>
2026-08-05 08:23:08 +0200

    Development: se finaliza desarrollo mantenimiento de prestamos y reservas

--- AtlasLMS.Blazor/Features/Bookings/Pages/BookingMaintenanceCreate.razor.cs
+++ AtlasLMS.Blazor/Features/Bookings/Pages/BookingMaintenanceCreate.razor.cs
@@ -1,6 +1,9 @@
 ﻿using AtlasLMS.Blazor.Features.Bookings.Contracts;
+using AtlasLMS.Blazor.Features.Books.Contracts;
+using AtlasLMS.Blazor.Features.Users.Contracts;
 using AtlasLMS.Blazor.Security.Contracts;
 using AtlasLMS.Shared.DTOs.Create;
+using AtlasLMS.Shared.DTOs.Read;
 
 using BlazorBootstrap;
 
@@ -11,12 +14,24 @@ namespace AtlasLMS.Blazor.Features.Bookings.Pages;
 public partial class BookingMaintenanceCreate
 {
     [Inject] public required IBookingService bookingService { get; set; }
+    [Inject] public required IBookService BookService { get; set; }
+    [Inject] public required IUserService UserService { get; set; }
+    [Inject] public required IAtlasExceptionHandler AtlasExceptionHandler { get; set; }
     [Inject] public required ToastService ToastService { get; set; }
     [Inject] public required NavigationManager NavigationService { get; set; }
-    [Inject] public required IAtlasExceptionHandler AtlasExceptionHandler { get; set; }
 
+    private List<BookReadDto> books = new();
+    private List<UserReadDto> users = new();
     private BookingCreateDto booking = new();
     private bool currentPost = false;
+
+    #region OnInitialized---------------------------------------------------------------------
+    protected override async Task OnInitializedAsync()
+    {
+        books = (await BookService.GetBooksAsync()).ToList();
+        users = (await UserService.GetUsersAsync()).ToList();
+    }
+    #endregion
 
     #region Actions-----------------------------------------------------------------------
     private void HandleCancel() => NavigationService.NavigateTo("/bookings");
--- AtlasLMS.Blazor/Features/Loans/Pages/LoanMaintenanceCreate.razor.cs
+++ AtlasLMS.Blazor/Features/Loans/Pages/LoanMaintenanceCreate.razor.cs
@@ -1,6 +1,9 @@
-﻿using AtlasLMS.Blazor.Features.Loans.Contracts;
+﻿using AtlasLMS.Blazor.Features.Books.Contracts;
+using AtlasLMS.Blazor.Features.Loans.Contracts;
+using AtlasLMS.Blazor.Features.Users.Contracts;
 using AtlasLMS.Blazor.Security.Contracts;
 using AtlasLMS.Shared.DTOs.Create;
+using AtlasLMS.Shared.DTOs.Read;
 
 using BlazorBootstrap;
 
@@ -11,15 +14,28 @@ namespace AtlasLMS.Blazor.Features.Loans.Pages;
 public partial class LoanMaintenanceCreate
 {
     [Inject] public required ILoanService LoanService { get; set; }
+    [Inject] public required IBookService BookService { get; set; }
+    [Inject] public required IUserService UserService { get; set; }
+    [Inject] public required IAtlasExceptionHandler AtlasExceptionHandler { get; set; }
     [Inject] public required ToastService ToastService { get; set; }
     [Inject] public required NavigationManager NavigationService { get; set; }
-    [Inject] public required IAtlasExceptionHandler AtlasExceptionHandler { get; set; }
 
     private LoanCreateDto loan = new();
+    private List<BookReadDto> books = new();
+    private List<UserReadDto> users = new();
     private bool currentPost = false;
+
+    #region OnInitialized---------------------------------------------------------------------
+    protected override async Task OnInitializedAsync()
+    {
+        books = (await BookService.GetBooksAsync()).ToList();
+        users = (await UserService.GetUsersAsync()).ToList();
+    }
+    #endregion
 
     #region Actions------------------------------------------------------------------------------------
     private void HandleCancel() => NavigationService.NavigateTo("/loans");
+    private void WhenChangeBook() => Console.WriteLine($"Libro selecionado: {loan.BookID}");
     private async Task HandleSaveLoan(LoanCreateDto loan)
     {
         currentPost = true;
