5c0ebb948082b96524e2938b681b80f924988c58
Unai Villar López <unaidevcoding@gmail.com>
2026-08-06 14:56:53 +0200

    Improvement: se controla visibilidad de acciones segun la feature

---
+++ AtlasLMS.Blazor/Shared/Components/AtlasGridActions.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace AtlasLMS.Blazor.Shared.Components;
+
+public partial class AtlasGridActions<TID>
+{
+    #region Parameters-----------------------------------------------------------------
+    [Parameter] public TID? ID { get; set; }
+    [Parameter] public bool ShowView { get; set; } = true;
+    [Parameter] public bool ShowDelete { get; set; } = true;
+    [Parameter] public bool ShowEdit { get; set; } = true;
+    [Parameter] public EventCallback<TID> OnDelete { get; set; }
+    [Parameter] public EventCallback<TID> OnView { get; set; }
+    [Parameter] public EventCallback<TID> OnEdit { get; set; }
+    #endregion
+}
